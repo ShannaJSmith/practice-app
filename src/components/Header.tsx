38e9327ce0382df41358b4f9a5079451e21f99b4
Shanna Smith <shannasmith@Mac.phub.net.cable.rogers.com>
2025-10-21 20:56:21 -0400

    chore: add Header component and update homepage layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="text-center">
+      <h1 className="text-5xl font-bold mb-2">Shanna Jennelle Smith</h1>
+      <p className="text-xl font-light">
+        Fullstack Developer | React, TypeScript, Next.js, WordPress, Tailwind
+      </p>
+    </header>
+  );
+}
